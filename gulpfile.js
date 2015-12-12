16233fc043c37a2b9ad34052a180a2e41daeb72b
Robert Chang <cht8687@gmail.com>
2015-12-13 06:24:30 +1100

    0.1.7

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,53 @@
 
 var destFolderPath = '../cht8687.github.io/react-webpack-starter-kit/example';
 var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var shell = require('gulp-shell');
-var gulptil = require('gulp-util');
-var gulpif = require('gulp-if');
 var args = require('yargs').argv;
-var gulpprint = require('gulp-print');
+var runSequence = require('run-sequence');
+var $ = require('gulp-load-plugins')({lazy: true});
+
+// var shell = require('gulp-shell');
+// var gulptil = require('gulp-util');
+// var gulpif = require('gulp-if');
+// var gulpprint = require('gulp-print');
+
+
 
 // Variables
-var version = '0.1.5';
+var version = '0.1.7';
 var gitCommit = `git commit -m ${version}`;
 var gitTag = `git tag v${version}`;
 
-gulp.task('npm-pub',shell.task([
+gulp.task('npm-pub', $.shell.task([
     `npm publish`
   ]
 ));
 
 gulp.task('moveToDest',['npm-pub'], function(){
-  gulptil.log('Moving example files to github.io...');
+  $.util.log('Moving example files to github.io...');
   return gulp.src('dist/*.js')
-    .pipe(gulpif(args.verbase, gulpprint()))
-    .pipe(gulp.dest(destFolderPath))
+    .pipe(gulp.dest(destFolderPath))  
 });
 
 gulp.task('pub',['moveToDest'], function(){
   return runSequence('push-to-git');
 });
 
-gulp.task('git-add', shell.task([
+gulp.task('git-add', $.shell.task([
     `git add -A`
   ]
 ));
 
-gulp.task('git-commit', shell.task([
+gulp.task('git-commit', $.shell.task([
     gitCommit
   ]
 ));
 
-gulp.task('git-push', shell.task([
+gulp.task('git-push', $.shell.task([
   `git push`,
  ]
 ))
 
-gulp.task('git-tag', shell.task([
+gulp.task('git-tag', $.shell.task([
    gitTag,
    `git push --tags`
   ]
